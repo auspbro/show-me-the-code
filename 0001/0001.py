7c2a8c18b156facdcf10d6300b312225c0cdaad2
Ryan Xue <auspbro@gmail.com>
2018-05-31 23:20:02 +0800

    Update script

--- 0001/0001.py
+++ 0001/0001.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 __author__ = 'Ryan'
-
 """
-**第 0002 题:** 将 0001 题生成的 200 个激活码（或者优惠券）保存到 **MySQL** 关系型数据库中。 
+第 0001 题： 做为 Apple Store App 独立开发者，你要搞限时促销，为你的应用生成激活码（或者优惠券），
+使用 Python 如何生成 200 个激活码（或者优惠券）？
 """
-
 
 import uuid
 
 
 def create_code(num, length):
-#生成”num“个激活码，每个激活码含有”length“位
+    # 生成”num“个激活码，每个激活码含有”length“位
     result = []
     while True:
         uuid_id = uuid.uuid1()
@@ -21,5 +20,6 @@ def create_code(num, length):
         if len(result) == num:
             break
     return result
+
 
 print create_code(200, 20)
