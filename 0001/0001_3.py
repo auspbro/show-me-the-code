c8844f7b3742d07e45f0f569c2d9c1567efe1cf4
Ryan Xue <auspbro@gmail.com>
2018-06-01 22:50:13 +0800

    fine tune script

--- 0001/0001_3.py
+++ 0001/0001_3.py
@@ -7,15 +7,18 @@
 import string
 import random
 
-KEY_LEN = 50
+KEY_LEN = 20
 KEY_NUM = 200
+
 
 def base_str():
     return (string.letters + string.digits)
 
+
 def key_gen():
     keylist = [random.choice(base_str()) for i in range(KEY_LEN)]
     return("".join(keylist))
+
 
 def key_num(num, result=None):
     if result is None:
@@ -24,9 +27,11 @@ def key_num(num, result=None):
         result.append(key_gen())
     return result
 
+
 def print_key(num):
     for i in key_num(num):
         print i
+
 
 if __name__ == '__main__':
     print_key(KEY_NUM)
